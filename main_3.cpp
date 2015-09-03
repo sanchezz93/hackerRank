08a8ba6abea9be1fb91f6ea2f28fbec142898739
Alejandro Sanchez <alejandro.s1993@gmail.com>
2015-09-03 18:25:55 -0500

    Started hackerrank again

---
+++ main_3.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  hackerrank_3
+//
+//  Created by Alejandro Sanchez on 1/26/15.
+//  Copyright (c) 2015 Alejandro_Sanchez. All rights reserved.
+//
+
+#include <iostream>
+
+using namespace std;
+
+
+int main() {
+    
+    cout << "Hello, World!\n";
+    return 0;
+}
---
+++ staircase.cpp
@@ -0,0 +1,25 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <iomanip> 
+#include <algorithm>
+
+using namespace std;
+
+
+int main() {
+ 	double iSize;
+ 	int input;
+	cin >> iSize;
+ 	for(int i=0;i < iSize;i++){
+ 		for(int j=iSize;j > 0;j--){
+ 			cout << "#";
+ 		}
+ 		cout << endl;
+ 	}
+
+ 	
+
+ 	return 0;
+ }
